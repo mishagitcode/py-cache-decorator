e265df87c27cddd61df666ada58486c32cf59977
Misha Zubritsky <zubritsky.misha@gmail.com>
2025-08-07 19:35:27 +0300

    implemented py-cache-decorator v2

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     stored_cache = {}
 
-    def validation(*args) -> Callable:
-        if args in stored_cache:
+    def wrapper(*args, **kwargs) -> Any:
+        key = (args, frozenset(kwargs.items()))
+        if key in stored_cache:
             print("Getting from cache")
-            return stored_cache[args]
+            return stored_cache[key]
         else:
             print("Calculating new result")
-            stored_cache[args] = func(*args)
-            return stored_cache[args]
+            stored_cache[key] = func(*args, **kwargs)
+            return stored_cache[key]
 
-    return validation
+    return wrapper
